3d4d97b0284ce577d2484c2ca710973b5b911669
Jeongyun Kim <0higherfurtherfaster0@gmail.com>
2026-04-26 16:51:36 +0900

    Add: Anti Debugging Framework

---
+++ AntiDebuggingFramework_SystemSecurity/Anti-Debugging Framework_Client/Client_2115227_김정윤.cpp
@@ -0,0 +1,130 @@
+﻿// Assignment 8—Anti-debugging framework 구현
+// IT공학전공 김정윤(2115227)
+
+
+// Client.exe
+// 1. 중요한 기능
+// 2. 부모 프로세스(Launcher.exe) 체크 기능
+// 3. 디버깅 여부 점검 - Launcher로 디버깅 중이어야 함(선점)
+
+#include <Windows.h>
+#include <stdio.h>
+#include <TlHelp32.h>
+#include <string.h>
+
+bool checkParentProcess() {
+	// Launcher에 의해 호출됨
+	// 프로그램 실행 즉시 부모 프로그램을 검사하여 부모 프로세스 이름이 Launcher.exe일 때만 프로그램을 정상 실행
+	// 윈도우즈 탐색기에서 더블 클릭했을 때 실행되면 안됨
+
+	DWORD clientPID = GetCurrentProcessId();				// client의 pid
+	DWORD parentPID = 0;										// launcher의 pid
+
+	printf("[CLIENT] Client.exe의 PID는 %lu입니다.\n", clientPID);
+
+
+	HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (h == INVALID_HANDLE_VALUE) return false;
+
+	PROCESSENTRY32W pe = { 0 };
+	pe.dwSize = sizeof(PROCESSENTRY32W);
+
+	// 프로세스 목록을 처음부터 순회 -> clientPID를 통해
+	// 부모 프로세스(Launcher.exe)의 PID를 찾아냄
+	if (Process32FirstW(h, &pe)) {
+		do {
+			// 순회 중 clientPID를 찾았다면 변수에 부모 PID 저장
+			if (pe.th32ProcessID == clientPID) {
+				parentPID = pe.th32ParentProcessID;
+				printf("[CLIENT] Client.exe의 부모 프로세스의 PID는 %lu입니다.\n", parentPID);
+				break;
+			}
+		} while (Process32NextW(h, &pe));		// 다음 프로세스로 이동
+	}
+
+	if (parentPID == 0) {
+		printf("[CLIENT] 부모 프로세스를 찾을 수 없습니다.\n");
+		CloseHandle(h);
+		return false;
+	}
+
+	// 프로세스 목록을 처음부터 순회 -> parentPID를 통해
+	// 부모 프로세스의 이름을 확인
+	bool isLauncher = false;
+
+	if (Process32FirstW(h, &pe)) {
+		do {
+			if (pe.th32ProcessID == parentPID) {
+				printf("[CLIENT] 부모 프로세스의 이름은 %ws입니다.\n", pe.szExeFile);
+				if (_wcsicmp(pe.szExeFile, L"Launcher.exe") == 0) {
+					isLauncher = true;
+				}
+				else {
+					printf("[CLIENT] 부모 프로세스의 이름은 %ws입니다.\n", pe.szExeFile);
+				}
+				break;
+			}
+		} while (Process32NextW(h, &pe));
+	}
+	CloseHandle(h);
+	return isLauncher;
+}
+
+int is_running = 1;			// Thread Flag
+HANDLE hThread = NULL;
+
+__inline BOOL checkBeingDebugged() {
+	__asm {
+		mov eax, dword ptr fs : [0x30]				// Thread Environment Block -> Process Environment Block
+		movzx eax, byte ptr ds : [eax + 0x02]		// Process Environment Block -> BeingDebugged
+	}
+}
+
+DWORD WINAPI debugger_watchdog(void* arg) {
+	printf("[CLIENT] 디버깅 탐지 시작합니다..\n");
+	while (is_running) {
+		if (!checkBeingDebugged()) {
+			printf("[CLIENT] 디버깅 당하고 있지 않습니다. 프로그램을 종료합니다.\n");
+			ExitProcess(1);		// 프로세스 종료
+		}
+		else Sleep(1000);
+	}
+	printf("[CLIENT] 스레드를 종료합니다.\n");
+	return 0;
+}
+
+int main() {
+
+	Sleep(1000);
+
+	if (!checkParentProcess()) {
+		printf("[CLIENT] Launcher.exe에 의해 실행되고 있지 않습니다. 프로그램을 종료합니다.\n");
+		ExitProcess(1);
+	}
+
+	if (!checkBeingDebugged()) {
+		printf("[CLIENT] Launcher.exe에 의해 디버깅 당하고 있지 않습니다. 프로그램을 종료합니다.\n");
+		ExitProcess(1);
+	}
+
+	hThread = CreateThread(0, 0, debugger_watchdog, 0, 0, 0);
+
+	printf("[CLIENT] Launcher.exe.에 의해 정상 실행되었습니다.\n");
+	// 1. 중요한 기능
+	char strbuffer[256];
+	while (1) {
+		printf("[CLIENT] 문자열을 입력하시오\n");
+		scanf_s("%s", strbuffer);
+		if (strcmp(strbuffer, "exit") == 0) {
+			printf("[CLIENT] 프로그램을 종료합니다.\n");
+			break;
+		}
+		printf("[CLIENT] %s\n", strbuffer);
+	}
+
+	is_running = 0;
+	WaitForSingleObject(hThread, 1000);
+	CloseHandle(hThread);
+
+	return 0;
+}
---
+++ AntiDebuggingFramework_SystemSecurity/Anti-Debugging Framework_Launcher/Launcher_2115227_김정윤.cpp
@@ -0,0 +1,140 @@
+﻿// Assignment 8—Anti-debugging framework 구현
+// IT공학전공 김정윤(2115227)
+
+
+// Launcher.exe
+// 1. 디버깅 여부 점검
+// 2. Client 프로그램 실행 - CreateProcess
+// 3. Client 프로그램 디버깅 - Debugger
+
+#include <windows.h>
+#include <stdio.h>
+
+#pragma comment(linker, "/INCLUDE:__tls_used")			// 프로젝트(링커) 설정. TLS 사용함
+#pragma comment(linker, "/INCLUDE:_pCallBacks")		// pCallBacks 변수를 링커에 전달
+
+
+// 1. 디버깅 여부 점검
+// watchdog 함수를 재활용하여 디버깅을 탐지하였다.
+
+int is_running = 1;			// Thread Flag
+HANDLE hThread = NULL;
+
+__inline BOOL IsDebuggerPresentAsm() {
+	__asm {
+		mov eax, dword ptr fs : [0x30]				// Thread Environment Block -> Process Environment Block
+		movzx eax, byte ptr ds : [eax + 0x02]		// Process Environment Block -> BeingDebugged
+	}
+}
+
+DWORD WINAPI debugger_watchdog(void* arg) {
+	printf("[LAUNCHER] 디버깅 탐지 시작합니다..\n");
+	while (is_running) {
+		if (IsDebuggerPresentAsm()) {
+			printf("[LAUNCHER] 디버깅 당하는 중입니다. 프로그램을 종료합니다.\n");
+			ExitProcess(1);		// 프로세스 종료
+		}
+		else Sleep(1000);
+	}
+	printf("[LAUNCHER] 스레드를 종료합니다.\n");
+	return 0;
+}
+
+// tls_callback도 재활용하여 프로세스를 실행하자마자 디버깅을 탐지하도록 하였다.
+void NTAPI TLS_CALLBACK1(PVOID DllHandle, DWORD Reason, PVOID Reserved) {
+
+	printf("[LAUNCHER] No. 1 with Reason=%u\n", Reason);
+
+	if (IsDebuggerPresentAsm()) {
+		printf("[LAUNCHER] 디버깅 당하는 중입니다. 프로그램을 종료합니다.\n");
+		ExitProcess(1);		// 프로세스 종료
+	}
+
+	if (Reason == 1)
+		hThread = CreateThread(0, 0, debugger_watchdog, 0, 0, 0);		// debugger_watchdog을 실행할 새로운 쓰레드 생성
+	else
+		Sleep(100);
+}
+
+#pragma data_seg(".CRT$XLX")
+extern "C" PIMAGE_TLS_CALLBACK pCallBacks[] = { TLS_CALLBACK1, 0 };
+#pragma data_seg()
+
+
+int main() {
+
+	printf("[LAUNCHER] 프로그램을 시작했습니다.\n");
+
+	// 2. Client.exe 실행
+	// CreateProcessA로 Client.exe를 실행한다. 
+	STARTUPINFOA si = { 0 };
+	PROCESS_INFORMATION pi = { 0 };
+	si.cb = sizeof(si);
+	char cmdLine[] = "Client.exe";
+
+	if (!CreateProcessA(NULL, cmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+		printf("[LAUNCHER] Client.exe 실행을 실패했습니다.\n");
+		return 0;
+	}
+
+	printf("[LAUNCHER] Client.exe를 실행하였습니다. (PID: %d)\n", pi.dwProcessId);
+
+	// 3. Client.exe 디버깅
+	// DebugActiveProcess로 Launcher를 Client에 부착하여 디버깅한다.
+	if (!DebugActiveProcess(pi.dwProcessId)) {
+		printf("[LAUNCHER] Client.exe 디버거 부착에 실패하였습니다.\n");
+		return 0;
+	}
+
+	printf("[LAUNCHER] Client.exe 디버거 부착에 성공하였습니다.\n");
+
+	// 디버깅 과정에서 발생하는 각종 DEBUG_EVENT들에 대한 처리를 다루는 무한 루프이다.
+	DEBUG_EVENT de;
+	BOOL isDebugging = true;
+	DWORD dwContinueStatus = DBG_CONTINUE;
+
+	// Debugger's Main Loop
+	while (isDebugging) {
+		if (!WaitForDebugEvent(&de, INFINITE)) break;
+		
+		dwContinueStatus = DBG_CONTINUE;
+		switch (de.dwDebugEventCode) {
+			case EXCEPTION_DEBUG_EVENT:	
+				break;
+			case CREATE_THREAD_DEBUG_EVENT:
+				break;
+			case CREATE_PROCESS_DEBUG_EVENT:
+				break;
+			case EXIT_THREAD_DEBUG_EVENT:
+				break;
+
+			// 부착한 프로세스가 종료했을 때((Client.exe에 exit이 입력됐을 경우)는 Launcher.exe도 함께 정상 종료시키도록 하였다.
+			case EXIT_PROCESS_DEBUG_EVENT:
+				printf("[LAUNCHER] Client.exe가 종료되었습니다.\n");
+				isDebugging = false;
+				break;
+			case LOAD_DLL_DEBUG_EVENT:
+				break;
+			case UNLOAD_DLL_DEBUG_EVENT:
+				break;
+			case OUTPUT_DEBUG_STRING_EVENT:
+				break;
+			case RIP_EVENT:
+				break;
+			default:
+				break;
+		}
+		
+		if (isDebugging) ContinueDebugEvent(de.dwProcessId, de.dwThreadId, dwContinueStatus);
+	}
+
+	is_running = 0;
+
+	if(hThread) WaitForSingleObject(hThread, 1000);
+	CloseHandle(hThread);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	printf("[LAUNCHER] 정상 종료되었습니다.\n");
+	return 0;
+}
